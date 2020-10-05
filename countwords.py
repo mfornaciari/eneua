a143928111201dd7d3a1463aea033fcaf11f3748
Marco Fornaciari <mfornaciari@gmail.com>
2020-10-05 16:37:04 -0300

    Mudança na estrutura de diretórios; adaptação do código para todos os anais; correção da limpeza de pontuação; remoção temporária do código que ignora elementos pré-textuais

--- countwords.py
+++ countwords.py
@@ -1,21 +1,23 @@
 from string import punctuation
 from text_cleaner import text_cleaner
 
-with open('anais1.txt', encoding='utf-8') as f, open('ignore.txt', encoding='utf-8') as f2:
-    text = f.readlines() # Texto, dividido em lista de linhas
-    ignore = f2.read().lower() # Lista de termos a ignorar, em minúsculas
-
-text = text_cleaner(text) # Texto limpo, dividido em lista de linhas
+file_name = input('Digite o nome do arquivo sem a extensão .txt: ')
+text = text_cleaner(file_name) # Texto limpo, dividido em lista de linhas
 words = [] # lista de palavras no texto
 
-for line in text: # Executa operações em cada linha do texto
-    for char in punctuation: # Remove pontuação
-        line = line.replace(char, ' ')
+with open('ignore.txt', encoding='utf-8') as f2:
+    ignore = f2.read().lower() # Lista de termos a ignorar, em minúsculas
+
+for line in text:
+    for char in punctuation + '“”‘’': # Remove pontuação
+        if char in line:
+            line = line.replace(char, ' ')
     line = line.lower() # Transforma linha em minúsculas
     line = line.split() # Divide linha em palavras
 
     for word in line:
-        if word in ignore or len(word) == 1: # Ignora palavras na lista de ignoradas ou letras avulsas
+        # Ignora: 1) Letras avulsas; 2) números; 3) palavras na lista de ignoradas
+        if len(word) == 1 or word.isnumeric() or word in ignore:
             continue
         words.append(word) # Adiciona palavra à lista de palavras
 
@@ -27,9 +29,9 @@ for word in words:
         continue
     wordcount[word] += 1 # Soma 1 ao valor da chave da palavra no dicionário
 
-pairs = sorted(wordcount.items(),
+pairs = sorted(wordcount.items(), # Organiza lista de palavras por quantidade de ocorrências
                 key=lambda pair:pair[1],
                 reverse=True)
 
-for pair in pairs[:20]:
+for pair in pairs[:20]: # Exibe palavras e número de ocorrências
     print(pair[0], pair[1])
--- text_cleaner.py
+++ text_cleaner.py
@@ -1,23 +1,39 @@
-def text_cleaner(text):
+def text_cleaner(file_name):
     output = [] # Lista que conterá o texto limpo, dividido em linhas
-    adding = False # Flag para começar a adicionar linhas
     skip = False # Flag para pular marca d'água
+    skip_count = 0 # Contador de linhas a pular
+
+    with open('Anais/' + file_name + '.txt', encoding='utf-8') as f:
+        text = f.readlines() # Texto, dividido em lista de linhas
 
     for line in text:
-        if line.startswith('SUMÁRIO'): # Começa a adicionar linhas a partir do sumário
-            adding = True
+        if line.startswith('\n'): # Pula linha em branco
             continue
-
-        if adding:
-            if line.startswith('\n'): # Pula linha em branco
+        
+        if file_name == 'anais6':
+            if line.startswith('Anais d'): # Pula única linha da marca d'água
                 continue
-            elif line.startswith('Anais'): # Pula primeira linha da marca d'água
+
+        elif file_name in ('anais1', 'anais4'):
+            if line.startswith('Anais'): # Pula primeira linha da marca d'água
                 skip = True
                 continue
-            elif skip: # Pula segunda linha da marca d'água
+            if skip: # Pula segunda linha da marca d'água
                 skip = False
                 continue
-            
-            output.append(line) # Adiciona linha a uma lista
+        
+        elif file_name in ('anais2', 'anais3'):
+            if line.startswith('ANAIS DO'): # Pula primeira linha da marca d'água
+                skip = True
+                skip_count += 1
+                continue
+            while skip and skip_count <= 2: # Pula linhas da marca d'água até pular 3 linhas
+                skip_count += 1
+                continue
+            else: # Reseta flag e contador após pular todas as linhas da marca d'água
+                skip = False
+                skip_count = 0
+        
+        output.append(line) # Adiciona linha a uma lista
         
     return output
