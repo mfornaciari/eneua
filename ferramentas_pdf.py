2c053e0e2684480fa31d8737327645d8be871cfe
Marco Fornaciari <mfornaciari@gmail.com>
2021-11-07 10:32:34 -0300

    Exceção caso se tente extrair pág. ausente do pdf.

--- ferramentas_pdf.py
+++ ferramentas_pdf.py
@@ -1,27 +1,45 @@
 'Ferramentas para extrair texto de PDF.'
 
 from typing import Iterator
-from pdfminer.high_level import extract_pages
 from pdfminer.layout import LAParams, LTTextBoxHorizontal
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfinterp import resolve1
+from pdfminer.high_level import extract_pages
 
 
-def pegar_pags(caminho_arquivo: str, numeros: list | None = None) -> Iterator:
+def pegar_pags(caminho_arquivo: str, numeros: list | None = None) -> tuple:
     '''
     Extrai páginas de PDF.\n
-    Retorna iterator de objetos LTPage.
+    Retorna tupla de objetos LTPage.
     '''
+    with open(caminho_arquivo, 'rb') as arquivo:
+        analisador = PDFParser(arquivo)
+        documento = PDFDocument(analisador)
+        total_paginas = resolve1(documento.catalog['Pages'])['Count'] - 1
+
+        assert not numeros or numeros and not any(
+            [numero > total_paginas for numero in numeros])
 
     print('Extraindo páginas...')
     # laparams: Parâmetros para análise de layout do documento.
     # Necessário p/ extração correta de blocos de texto.
     laparams = LAParams(word_margin=0.5, line_margin=1, boxes_flow=None)
-    pags_ltpage = extract_pages(
+    iterator_pags = extract_pages(
         caminho_arquivo, page_numbers=numeros, laparams=laparams)
+
+    tupla_pags = ()
+    for numero, pagina in enumerate(iterator_pags, start=1):
+        print(f"Página {numero} extraída.")
+        tupla_pags += (pagina,)
+
     print('Páginas extraídas.')
-    return pags_ltpage
+    return tupla_pags
 
 
-def pegar_blocos_texto(pags_ltpage: Iterator) -> dict:
+def pegar_blocos_texto(tupla_pags: tuple) -> dict:
     '''
     Extrai blocos de texto.\n
     Retorna dict com pares = número da página (int):
@@ -32,7 +50,7 @@ def pegar_blocos_texto(pags_ltpage: Iterator) -> dict:
     dict_blocos = {}
     num_pag = 0
 
-    for pag in pags_ltpage:
+    for pag in tupla_pags:
         num_pag += 1
         blocos_pag = []
 
@@ -42,7 +60,7 @@ def pegar_blocos_texto(pags_ltpage: Iterator) -> dict:
                 blocos_pag.append(bloco)
 
         dict_blocos[num_pag] = blocos_pag
-        print(f'Blocos da pág. {num_pag} extraídos.')
+        print(f'Blocos da página {num_pag} extraídos.')
 
     print('Blocos de texto extraídos.')
     return dict_blocos
@@ -65,7 +83,7 @@ def pegar_texto(dict_blocos: dict) -> dict:
             texto_pag += f'\n{texto_bloco}\n'
 
         dict_texto[num_pag] = texto_pag
-        print(f'Texto da pág. {num_pag} extraído.')
+        print(f'Texto da página {num_pag} extraído.')
 
     print('Texto completo extraído.')
     return dict_texto
--- main.py
+++ main.py
@@ -4,20 +4,36 @@ import ferramentas_anais as fa
 import gerenciamento_entrada as ge
 
 
-# Definindo caminho relativo p/ anais
-caminho_diretorio_atual = path.dirname(__file__)
-entrada_numero_anais = ge.pegar_num_anais()
-caminho_arquivo_anais = path.join(
-    caminho_diretorio_atual, 'anais', f'anais_{entrada_numero_anais}.pdf')
+def pegar_caminho_relativo(numero_anais):
+    # Definindo caminho relativo p/ anais
+    caminho_diretorio_atual = path.dirname(__file__)
+    return path.join(caminho_diretorio_atual, 'anais', f'anais_{numero_anais}.pdf')
 
-# Selecionando págs.
-nums_pag = ge.pegar_nums_pag()
-# Extraindo texto
-texto_pags = fp.extrair(caminho_arquivo_anais, nums_pag)
-# Limpando texto
-# fa.limpar_texto(texto_pags)
-# Contando palavras e organizando contagem
-contagem_organizada = fa.contar(texto_pags)
+
+def gerar_texto(caminho_arquivo):
+    try:
+        # Selecionando págs.
+        nums_pag = ge.pegar_nums_pag()
+        # Extraindo texto
+        # Limpando texto
+        # fa.limpar_texto(texto_pags)
+        return fp.extrair(caminho_arquivo, nums_pag)
+
+    except AssertionError:
+        print('O arquivo escolhido não possui uma ou mais das páginas definidas.\n')
+        return gerar_texto(caminho_arquivo_anais)
+
+
+def gerar_contagem(texto_pags):
+    # Contando palavras e organizando contagem
+    return fa.contar(texto_pags)
+
+
+# Executando processo principal
+entrada_numero_anais = ge.pegar_num_anais()
+caminho_arquivo_anais = pegar_caminho_relativo(entrada_numero_anais)
+texto_pags = gerar_texto(caminho_arquivo_anais)
+contagem_organizada = gerar_contagem(texto_pags)
 
 # TESTE
 fa.escrever_completo(texto_pags, entrada_numero_anais)
