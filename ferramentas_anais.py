f9a0565c06953f52b0514d2440a56b235bb3b930
Marco Fornaciari <mfornaciari@gmail.com>
2022-02-17 10:30:54 -0300

    REFAT: Otimizar ferramentas_anais
    Remoção de código desnecessário e condensação

--- ferramentas_anais.py
+++ ferramentas_anais.py
@@ -4,29 +4,12 @@ import re
 from string import punctuation
 
 
-# Limpa texto de cada pág. em um dict com pares = núm. da pág.: texto da pág.
-# def limpar_texto(dict_texto):
-#     print('Limpando texto...')
-
-#     for pag, texto in dict_texto.items():
-#         texto = re.sub(r'\n\s+', r'\n', texto)  # Remove linhas em branco
-#         texto = re.sub(r' +', r' ', texto)  # Remove espaços extras
-#         dict_texto[pag] = texto
-#         print(f'Texto da pág. {pag} limpo.')
-
-#     print('Texto completo limpo.')
-
-
 # Separa palavras do texto completo.
 # Retorna lista de palavras (str).
 def separar_palavras(dict_texto: dict) -> list:
     print('Separando palavras...')
-    palavras = []
-
-    for pag in dict_texto:
-        lista_palavras_pag = dict_texto[pag].split()
-        palavras.extend(lista_palavras_pag)
-
+    palavras = [
+        palavra for pag in dict_texto for palavra in dict_texto[pag].split()]
     print('Palavras separadas.')
     return palavras
 
@@ -35,26 +18,18 @@ def separar_palavras(dict_texto: dict) -> list:
 # Retorna lista de palavras (str).
 def limpar_palavras(palavras: list) -> list:
     print('Limpando palavras...')
-    palavras_limpas = []
     pontuacao = punctuation + '“”‘…–ºª'
-    lista_ignorar = []
     with open('ignore.txt', 'r', encoding='utf-8') as arquivo_ignorar:
         lista_ignorar = [palavra.strip('\n')
                          for palavra in arquivo_ignorar.readlines()]
+    palavras_limpas = []
 
     for palavra in palavras:
-        tem_digitos = any(caractere.isdigit() for caractere in palavra)
-
-        if tem_digitos:
-            # Checa se é um ano (4 dígitos seguidos)
-            ano = re.search(r'[0-9]{4}', palavra)
-
-            if not ano:
-                # Remove todos os dígitos
-                palavra = re.sub(r'[0-9]+', r'', palavra)
-
+        # Checa se palavra é um ano (4 dígitos seguidos)
+        if not re.search(r'[0-9]{4}', palavra):
+            # Remove todos os dígitos da palavra se ela não for um ano
+            palavra = re.sub(r'[0-9]+', r'', palavra)
         palavra = palavra.strip(pontuacao)
-
         if palavra.lower() not in lista_ignorar and len(palavra) > 1:
             palavras_limpas.append(palavra.upper())
 
@@ -63,8 +38,9 @@ def limpar_palavras(palavras: list) -> list:
 
 
 # Conta número de ocorrências de cada palavra.
-# Retorna dict onde pares = palavra: núm. de ocorrências.
-def contar_palavras(palavras_limpas: list) -> dict:
+# Retorna lista de tuplas organizada por número decrescente de ocorrências.
+# Tupla[0] = palavra (str) e tupla[1] = número de ocorrências (int).
+def contar_palavras(palavras_limpas: list) -> list:
     print('Contando palavras...')
     contagem = {}
 
@@ -73,23 +49,14 @@ def contar_palavras(palavras_limpas: list) -> dict:
             1 if palavra in contagem else 1
 
     print('Todas as palavras contadas.')
-    return contagem
-
-
-# Organiza contagem de palavras por número de ocorrências.
-# Retorna lista de tuplas, onde tupla[0] = palavra (str) e tupla[1] = núm. de ocorrências (int)
-def organizar_contagem(contagem: dict) -> list:
-    contagem_organizada = sorted(contagem.items(),
-                                 key=lambda par: par[1], reverse=True)
-    return contagem_organizada
+    return sorted(contagem.items(), key=lambda par: par[1], reverse=True)
 
 
 # Processo completo de contagem.
 def contar(dict_texto: dict) -> list:
     palavras = separar_palavras(dict_texto)
     palavras_limpas = limpar_palavras(palavras)
-    contagem = contar_palavras(palavras_limpas)
-    return organizar_contagem(contagem)
+    return contar_palavras(palavras_limpas)
 
 
 # Cria TXT com texto completo das págs. extraídas
