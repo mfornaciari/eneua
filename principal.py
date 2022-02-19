862223ec954f5a760b97273667d75bf926ae123b
Marco Fornaciari <mfornaciari@gmail.com>
2022-02-19 08:41:53 -0300

    REFAT: Reescrever definição de caminho p/ anais
    Adequando código a testes unitários

--- principal.py
+++ principal.py
@@ -1,30 +1,32 @@
 import os
-import gerenciamento_entrada as ge
+import entrada
 import ferramentas_pdf as fp
 import ferramentas_anais as fa
 import ferramentas_nuvem as fn
 
 
-# Definindo caminho relativo p/ anais
-numero_anais = ge.pegar_num_anais()
-diretorio_atual = os.path.dirname(__file__)
-arquivo_anais = os.path.join(
-    diretorio_atual, 'anais', f'anais_{numero_anais}.pdf')
+if __name__ == '__main__':
+    # Definindo caminho relativo p/ anais
+    num_anais = input('Digite o número dos anais (1-6): ')
+    while not entrada.validar_num_anais(num_anais):
+        num_anais = input('Digite o número dos anais (1-6): ')
+    diretorio_atual = os.path.dirname(__file__)
+    arquivo_anais = os.path.join(
+        diretorio_atual, 'anais', f'anais_{num_anais}.pdf')
 
-while True:
-    # Selecionando págs.
-    nums_pag = ge.pegar_nums_pag()
+    while True:
+        # Selecionando págs. a extrair
+        nums_pag = entrada.pegar_nums_pag()
+        try:
+            # Extraindo texto
+            texto_pags = fp.extrair(arquivo_anais, nums_pag)
+            break
+        except AssertionError:
+            print('O arquivo escolhido não possui uma ou mais das páginas definidas.\n')
+            continue
 
-    try:
-        # Extraindo texto
-        texto_pags = fp.extrair(arquivo_anais, nums_pag)
-        break
-    except AssertionError:
-        print('O arquivo escolhido não possui uma ou mais das páginas definidas.\n')
-        continue
-
-# Contando número de ocorrências de cada palavra
-contagem = fa.contar(texto_pags)
-# Gerando e exibindo nuvem de palavras
-nuvem = fn.gerar_nuvem(contagem)
-fn.exibir_nuvem(nuvem)
+    # Contando número de ocorrências de cada palavra
+    contagem = fa.contar(texto_pags)
+    # entradarando e exibindo nuvem de palavras
+    nuvem = fn.gerar_nuvem(contagem)
+    fn.exibir_nuvem(nuvem)
