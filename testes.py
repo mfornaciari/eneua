2f859bac9e736eb68f8fd0cbcee8d8570e412089
Marco Fornaciari <mfornaciari@gmail.com>
2022-02-19 16:34:32 -0300

    IMPLE: Implementar teste de validação de págs.
    Testa validação de páginas inseridas pelo usuário

--- testes.py
+++ testes.py
@@ -3,6 +3,8 @@ import entrada
 
 
 class TestarEntrada(unittest.TestCase):
+    def setUp(self):
+        self.pags_totais = 10
 
     def test_validar_num_anais(self):
         self.assertTrue(entrada.validar_num_anais('1'))
@@ -10,6 +12,34 @@ class TestarEntrada(unittest.TestCase):
         self.assertFalse(entrada.validar_num_anais('a'))
         self.assertFalse(entrada.validar_num_anais('-1'))
         self.assertFalse(entrada.validar_num_anais('1.2'))
+
+    def test_validar_pags(self):
+        self.assertFalse(entrada.validar_pags('', self.pags_totais))
+        self.assertFalse(entrada.validar_pags('a', self.pags_totais))
+        self.assertFalse(entrada.validar_pags(',', self.pags_totais))
+        self.assertFalse(entrada.validar_pags('1-,2', self.pags_totais))
+        self.assertFalse(entrada.validar_pags('2-1', self.pags_totais))
+        self.assertFalse(entrada.validar_pags('1-2-3', self.pags_totais))
+        self.assertFalse(entrada.validar_pags('1,2-', self.pags_totais))
+        self.assertFalse(entrada.validar_pags('12', self.pags_totais))
+        self.assertFalse(entrada.validar_pags('1,2-12', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1,1', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1,1-2', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1, ', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1,', self.pags_totais))
+        self.assertTrue(entrada.validar_pags(',1', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1,2', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1, 2', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1,2 ', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1 ,2', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1 , 2', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1-2', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1,2-3', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1,2-3,', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1,2 -3', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1,2- 3', self.pags_totais))
+        self.assertTrue(entrada.validar_pags('1, 2-3', self.pags_totais))
 
 
 if __name__ == '__main__':
