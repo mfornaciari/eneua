3358068c8f4d8572a8448877c14a349bc4b5c3e6
Marco Fornaciari <mfornaciari@gmail.com>
2022-02-21 13:20:16 -0300

    Refatorando TestarEntrada

--- testes.py
+++ testes.py
@@ -5,47 +5,53 @@ import ferramentas_pdf as fp
 
 
 class TestarEntrada(unittest.TestCase):
-    def setUp(self):
-        self.pags_totais = 10
-
     def test_validar_num_anais(self):
+        self.valores_falsos = ('7', 'a', '-1', '1.2')
+
         self.assertTrue(entrada.validar_num_anais('1'))
-        self.assertFalse(entrada.validar_num_anais('7'))
-        self.assertFalse(entrada.validar_num_anais('a'))
-        self.assertFalse(entrada.validar_num_anais('-1'))
-        self.assertFalse(entrada.validar_num_anais('1.2'))
+        for valor in self.valores_falsos:
+            self.assertFalse(entrada.validar_num_anais(valor))
 
     def test_validar_pags(self):
-        self.assertFalse(entrada.validar_pags(',,', self.pags_totais))
-        self.assertFalse(entrada.validar_pags('--', self.pags_totais))
-        self.assertFalse(entrada.validar_pags(',-', self.pags_totais))
-        self.assertFalse(entrada.validar_pags('a', self.pags_totais))
-        self.assertFalse(entrada.validar_pags(',', self.pags_totais))
-        self.assertFalse(entrada.validar_pags('1-,2', self.pags_totais))
-        self.assertFalse(entrada.validar_pags('2-1', self.pags_totais))
-        self.assertFalse(entrada.validar_pags('1-2-3', self.pags_totais))
-        self.assertFalse(entrada.validar_pags('1,2-', self.pags_totais))
-        self.assertFalse(entrada.validar_pags('11', self.pags_totais))
-        self.assertFalse(entrada.validar_pags('1,2-11', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1,1', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1,1-2', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('10', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1, ', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1,', self.pags_totais))
-        self.assertTrue(entrada.validar_pags(',1', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1,2', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1, 2', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1,2 ', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1 ,2', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1 , 2', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1-2', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1,2-3', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1,2-3,', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1,2 -3', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1,2- 3', self.pags_totais))
-        self.assertTrue(entrada.validar_pags('1, 2-3', self.pags_totais))
+        self.valores_verdadeiros = (
+            '',
+            '1,1',
+            '1,1-2',
+            '1',
+            '10',
+            '1, ',
+            ',1',
+            '1,2',
+            '1 ,2',
+            '1,2 ',
+            '1-2',
+            '1,2-3',
+            '1,2-3,',
+            '1,2 -3',
+            '1,2- 3',
+            '1, 2-3',
+        )
+        self.valores_falsos = (
+            ',',
+            '-',
+            ',,',
+            '--',
+            ',-',
+            '-,',
+            'a',
+            '1-',
+            '1-,2',
+            '2-1',
+            '1-2-3',
+            '1,2-',
+            '11',
+            '1,2-11',
+        )
+
+        for valor in self.valores_verdadeiros:
+            self.assertTrue(entrada.validar_pags(valor, 10))
+        for valor in self.valores_falsos:
+            self.assertFalse(entrada.validar_pags(valor, 10))
 
 
 class TestarFerramentasPDF(unittest.TestCase):
